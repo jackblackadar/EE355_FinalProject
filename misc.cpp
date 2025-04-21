714da0a260077a47b2d33bb7eb20177e19ae9d50
Jack Blackadar <blackada@usc.edu>
2025-04-21 14:40:40 -0700

    added helper to misc.cpp

--- misc.cpp
+++ misc.cpp
@@ -14,3 +14,13 @@ void printMe(string type){
         cout << endl << banner << endl << endl;
     }
 }
+
+string codeName(string fname, string lname) {
+    string result = fname + lname;
+
+    // remove spaces and lower
+    transform(result.begin(), result.end(), result.begin(), ::tolower);
+    result.erase(remove(result.begin(), result.end(), ' '), result.end());
+
+    return result;
+}
--- misc.h
+++ misc.h
@@ -4,9 +4,10 @@
 
 #include <iostream>
 #include <string>
+#include <algorithm>
 using namespace std;
 
 void printMe(string type);
-
+string codeName(string fname, string lname);
 
 #endif
